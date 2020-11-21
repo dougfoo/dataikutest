883409bc7b928254e77dc92cfcb8ba5ee881666d
admin <Doug.cha@gmail.com>
2020-11-21 18:07:11 +0900

    Saved recipe: TEST.compute_mycols

---
+++ recipes/compute_mycols.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+010006_hokkaido_covid19_patients = dataiku.Dataset("010006_hokkaido_covid19_patients")
+010006_hokkaido_covid19_patients_df = 010006_hokkaido_covid19_patients.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+mycols_df = 010006_hokkaido_covid19_patients_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+mycols = dataiku.Dataset("mycols")
+mycols.write_with_schema(mycols_df)
